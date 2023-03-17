49ff4b4fbe5256b1db11e70754521c90986cd07b
Shubham Singh <shubham2101@gmail.com>
2023-03-17 15:33:57 +0530

    reading and writing file system

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const hello = "Hello World"
-console.log(hello)
+const fs = require("fs");
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+// writing file
+const textOut = `This is what we all came for : ${textIn} .\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written");
