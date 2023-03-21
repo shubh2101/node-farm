1c05e58eff05ced168858c66a92069b7139eda40
Shubham Singh <shubham2101@gmail.com>
2023-03-22 00:30:20 +0530

    creating replaceTemp module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const replaceTemplate = require("./modules/replaceTemp.js")
 
 // synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -25,21 +26,6 @@ const http = require("http");
 // console.log("will read file");
 
 // creating server
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
